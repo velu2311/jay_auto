cadad80fed27aecb33bc26df10bf3cdc4d2b8918
velu2311 <55377987+velu2311@users.noreply.github.com>
2020-03-18 15:50:52 +0530

    conf.php
    files

---
+++ includes/conf/conf.php
@@ -0,0 +1,10 @@
+<?php 
+error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', 0);
+error_reporting(0);
+
+ini_set('include_path', get_include_path().PATH_SEPARATOR.dirname(__FILE__));
+define('DOC_ROOT',$_SERVER['DOCUMENT_ROOT']);
+
+define('OAUTH2_CLIENT_ID', 'in_iXak6whTpeQXCaKdA6qeG7s8a');
+define('OAUTH2_CLIENT_SECRET', 'LqaBMf0YS7RWT1jU3Oif7QhZMNoa');
